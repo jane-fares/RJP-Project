553713ccd93860966e5138a4284bbbc7533c893f
janefares <125896031+janefares@users.noreply.github.com>
2023-10-22 13:42:40 +0300

    first commit

---
+++ Core/Service/CustomerService.cs
@@ -0,0 +1,65 @@
+﻿using Core.Interface;
+using Core.Model;
+using Core.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Core.Service
+{
+    public class CustomerService : BaseService,ICustomerRepository
+    {
+        private ITransactionRepository _transactionRepository;
+        private IAccountRepository _accountRepository;
+        public CustomerService(ITransactionRepository transactionRepository,IAccountRepository accountRepository,Rjp_DatabaseContext _DatabaseContext):base(_DatabaseContext)
+        {
+            _transactionRepository = transactionRepository;
+            _accountRepository = accountRepository;
+        }
+        public CustomerModel Add(CustomerModel entity)
+        {
+            _DatabaseContext.Customers.Add(entity);
+            _DatabaseContext.SaveChanges();
+            return entity;
+        }
+
+        public IEnumerable<CustomerModel> GetAll()
+        {
+            return _DatabaseContext.Customers.Select(p=>(CustomerModel)p).ToList();
+        }
+
+        public CustomerModel GetById(int ?id=0)
+        {
+            return _DatabaseContext.Customers.Find(id);
+        }
+
+        public UserAccountModel GetCustomer(int id)
+        {
+            if (_DatabaseContext.Customers.Find(id) == null)
+                return null;
+            UserAccountModel userAccountModel = new UserAccountModel();
+            userAccountModel.customerModel = GetById(id);
+            var accounts = _accountRepository.GetAll().Where(p => p.CustomerId == id).ToList();
+            foreach(var account in accounts)
+            {
+                AccountTransactionModel accountTransactionModel = new AccountTransactionModel();
+                accountTransactionModel.transactionModels = _transactionRepository.GetAll().Where(tp => tp.AccountId == account.AccountId).ToList();
+                accountTransactionModel.accountModel = account;
+                userAccountModel.accountTrancationModels.Add(accountTransactionModel);
+            }
+            return userAccountModel;
+        }
+
+        public CustomerModel Update(CustomerModel entity)
+        {
+            var customer = GetById(entity?.CustomerId);
+            if (customer != null)
+            {
+                customer.FirstName = entity?.FirstName;
+                customer.LastName = entity?.LastName;
+                _DatabaseContext.SaveChanges();
+                return customer;
+            }
+            return null;
+        }
+    }
+}
---
+++ RJP/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Interface;
+using Core.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace RJP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private ICustomerRepository _customerRepository;
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(ILogger<CustomerController> logger,ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+            _logger = logger;
+
+        }
+
+        [HttpGet("{id}")]
+
+        public UserAccountModel Get(int id)
+        {
+
+            return _customerRepository.GetCustomer(id);
+        }
+        [HttpGet]
+        public IEnumerable<CustomerModel> All()
+        {
+            return _customerRepository.GetAll();
+        }
+    }
+}
